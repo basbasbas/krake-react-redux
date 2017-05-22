aca11800bba24d8837609fb89d80990ffe26e12c
basbasbas <sebastiaan.bosch@gmail.com>
2017-05-22 11:17:30 +0200

    Added reducers to load

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import locationReducer from './location'
+import pageReducer from './page'
 
 export const makeRootReducer = (asyncReducers) => {
   return combineReducers({
-    location: locationReducer,
-    ...asyncReducers
+	  location: locationReducer,
+	  page: pageReducer,
+	  ...asyncReducers
   })
 }
 
